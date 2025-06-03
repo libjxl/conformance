73f93512e75793468426ad6d79614a4d30da2618
Martin Bruse <zondolfin@gmail.com>
2025-06-03 13:16:43 +0200

    Changed the downloading to use curl instead of gsutil.
    This should enable runing the download and symlink script on more platforms.

--- scripts/download_and_symlink.sh
+++ scripts/download_and_symlink.sh
@@ -10,8 +10,17 @@ cd "$(dirname "$(realpath $0)")/.."
 
 mkdir -p .objects
 
-echo Fetching latest object state...
-gsutil -m rsync gs://jxl-conformance/objects/ .objects/
+echo -n Fetching latest object state
+for URL in $(curl -s https://storage.googleapis.com/storage/v1/b/jxl-conformance/o | grep mediaLink | sed -e 's/.*"mediaLink":\s*"\(.*\)",/\1/'); do
+	FILENAME=".objects/$(basename "${URL}" | sed -e 's/objects%2F\([^\?]\+\).*/\1/')"
+	if [ -f "${FILENAME}" ]; then
+		curl -s -z "${FILENAME}" -C - -o "${FILENAME}" "${URL}"
+	else
+		curl -s -o "${FILENAME}" "${URL}"
+	fi
+	echo -n .
+done
+echo done
 
 for t in $(find $TESTCASE_DIRECTORY -name test.json)
 do
